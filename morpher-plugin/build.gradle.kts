80ae0365121a2fd8b2cc149085cc7544c6d6bf9f
Canrad <1517807724@qq.com>
2025-06-14 21:18:19 +0800

    更新api的库版本

--- morpher-plugin/build.gradle.kts
+++ morpher-plugin/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    implementation("com.github.luiox:morpher-api:v1.2")
+    implementation("com.github.luiox:morpher-api:v1.3")
 }
 
 tasks.test {
